332b0ca91dcae77ecb764c39324f82851f302795
Christian Tsalidis <ctsalidis1999@gmail.com>
2022-09-28 15:52:33 +0200

    added rollerball ml environment to start with rts rl

---
+++ Unity_ML/Assets/ctsalidis/StocksTrading/Scripts/TradingAgent.cs
@@ -0,0 +1,9 @@
+using Unity.MLAgents;
+
+namespace ctsalidis.StocksTrading.Scripts {
+    public class TradingAgent : Agent {
+        public override void OnEpisodeBegin() {
+            base.OnEpisodeBegin();
+        }
+    }
+}
